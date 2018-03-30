ef4255ae06bd85ee87048d2ee135121bbc859290
Sandesh Gade <sandeshgade@gmail.com>
2018-03-29 20:11:21 -0400

    Backend implementation with OAuth

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+	google: {
+		clientID: process.env.GOOGLE_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_CLIENT_SECRET
+	},
+	mlab: {
+		dbURI: process.env.MLAB_DB_URI
+	},
+	cookieKey: process.env.COOKIE_KEY
+};
